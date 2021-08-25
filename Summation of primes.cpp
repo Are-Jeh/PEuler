8b6c04614931247ea63df526a52659523c320a8f
Rishav Jain <44065908+rishavjain4@users.noreply.github.com>
2021-08-25 20:03:06 +0530

    Add files via upload

---
+++ Summation of primes.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<bool>a(2000000, true);
+
+    for(int p=2; p*p<=2000000; p++)
+    {
+        if( a[p] )
+        {
+            for(int j = p*p; j<2000000; j+=p)
+            {
+                a[j] = false;
+            }
+        }
+    }
+    long long int sum=0;
+    for(int i=0;i<2000000;i++)
+    {
+        if(a[i])
+        sum+=i;
+    }
+    cout<<sum-1<<" ";
+
+}
+
+// array false; sieve of eratosthenes;
