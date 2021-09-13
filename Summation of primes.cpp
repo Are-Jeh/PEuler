d9564c7e6926a72c82536beae09d2d8a80212490
Rishav Jain <44065908+rishavjain4@users.noreply.github.com>
2021-09-13 19:11:35 +0530

    Update Summation of primes.cpp

--- Summation of primes.cpp
+++ Summation of primes.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 int main()
 {
-    vector<bool>a(2000000, true);
+    int n = 2000000;
+    vector<bool>a(n, true);
 
-    for(int p=2; p*p<=2000000; p++)
+    for(int p=2; p*p<=n; p++)
     {
         if( a[p] )
         {
-            for(int j = p*p; j<2000000; j+=p)
+            for(int j = p*p; j<n; j+=p)
             {
                 a[j] = false;
             }
         }
     }
     long long int sum=0;
-    for(int i=0;i<2000000;i++)
+    for(int i=2;i<n;i++)
     {
         if(a[i])
         sum+=i;
     }
-    cout<<sum-1<<" ";
+    cout<<sum<<" ";
 
 }
 
